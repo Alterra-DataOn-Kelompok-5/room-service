8c61ae1e62706e551a9f2afb786ae4086fcd246f
Samuel Christopher <samuel.christopher.santo@gmail.com>
2022-07-05 18:59:35 +0700

    feat: usage of godotenv and echo web framework

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func init() {
+	errGoEnv := godotenv.Load()
+	if errGoEnv != nil {
+		// log.Fatal("Error loading .env file")
+		panic(errGoEnv)
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	e := echo.New()
+	log.Fatal(e.Start(":" + os.Getenv("SERVICE_PORT")))
 }
