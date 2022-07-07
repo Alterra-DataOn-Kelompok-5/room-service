fe062bdd0897e00ace41fa3b1a144dcb113eee59
Samuel Christopher <samuel.christopher.santo@gmail.com>
2022-07-07 18:11:52 +0700

    fix: add preload(RoomLocations) to get foreign table room_locations

--- rooms/repository/rooms_repository.go
+++ rooms/repository/rooms_repository.go
@@ -17,30 +17,30 @@ func NewMysqlRoomsRepository(conn *gorm.DB) domain.RoomsRepository {
 
 func (m *mysqlRoomsRepository) FetchAll(ctx context.Context) (res []domain.Rooms, err error) {
 	var rooms []domain.Rooms
-	err = m.Db.WithContext(ctx).Model(&domain.Rooms{}).Preload("RoomTypes").Find(&rooms).Error
+	err = m.Db.WithContext(ctx).Model(&domain.Rooms{}).Preload("RoomTypes").Preload("RoomLocations").Find(&rooms).Error
 
 	return rooms, err
 }
 
 func (m *mysqlRoomsRepository) FetchByID(ctx context.Context, id int) (res domain.Rooms, err error) {
 	var room domain.Rooms
-	err = m.Db.WithContext(ctx).Model(&domain.Rooms{}).Preload("RoomTypes").Where("id = ?", id).First(&room).Error
+	err = m.Db.WithContext(ctx).Model(&domain.Rooms{}).Preload("RoomTypes").Preload("RoomLocations").Where("id = ?", id).First(&room).Error
 	return room, err
 }
 
 func (m *mysqlRoomsRepository) Store(ctx context.Context, r *domain.Rooms) error {
-	err := m.Db.WithContext(ctx).Model(&domain.Rooms{}).Preload("RoomTypes").Create(&r).Error
+	err := m.Db.WithContext(ctx).Model(&domain.Rooms{}).Preload("RoomTypes").Preload("RoomLocations").Create(&r).Error
 
 	return err
 }
 
 func (m *mysqlRoomsRepository) Update(ctx context.Context, r *domain.Rooms, id int) error {
-	err := m.Db.WithContext(ctx).Model(&domain.Rooms{}).Preload("RoomTypes").Where("id = ?", id).Updates(&r).Error
+	err := m.Db.WithContext(ctx).Model(&domain.Rooms{}).Preload("RoomTypes").Preload("RoomLocations").Where("id = ?", id).Updates(&r).Error
 
 	return err
 }
 
 func (m *mysqlRoomsRepository) Delete(ctx context.Context, id int) error {
-	err := m.Db.WithContext(ctx).Preload("RoomTypes").Where("id = ?", id).Delete(&domain.Rooms{}).Error
+	err := m.Db.WithContext(ctx).Preload("RoomTypes").Preload("RoomLocations").Where("id = ?", id).Delete(&domain.Rooms{}).Error
 	return err
 }
