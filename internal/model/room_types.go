5ba8cb01a1c958d842eba82e8a2b285fe591cfb1
azkafr92 <azkafr92@gmail.com>
2022-07-12 11:48:19 +0700

    bugfix: room type service & handler test cases

--- internal/model/room_types.go
+++ internal/model/room_types.go
@@ -2,7 +2,7 @@ package model
 
 type RoomTypes struct {
 	Common
-	RoomTypeName        string `json:"room_type_name"`
+	RoomTypeName        string `json:"room_type_name" gorm:"varchar;not_null;unique"`
 	RoomTypeMaxCapacity int    `json:"room_type_max_capacity"`
 	RoomTypeDesc        string `json:"room_type_desc" gorm:"type:text"`
 }
